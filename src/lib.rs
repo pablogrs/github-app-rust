2a83b095d3e199e48b49e917f9357d789a9a8e91
Pablo Sanchez <pablo.gon.sanchez@hpe.com>
2020-06-20 23:03:52 +0100

    read requests

--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-struct GithubEnvFile {
-    private_key: String,
-    webhook_secret: String,
-    app_identifier: String,
-} 
-
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,101 @@
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*;
-use std::fs;
-use dotenv;
+
+use futures::{future, Future, Stream};
+use hyper::{
+    client::HttpConnector, rt, service::service_fn, Body, Client, Request,
+    Response, Server, Method, StatusCode, header
+};
+
+use serde_json::{Value};
+
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    pretty_env_logger::init();
 
-    // Load .env properties file
-    dotenv::dotenv().ok();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
+    let addr = "127.0.0.1:7878".parse().unwrap();
+    type GenericError = Box<dyn std::error::Error + Send + Sync>;
+    type ResponseFuture = Box<dyn Future<Item = Response<Body>, Error = GenericError> + Send>;
+    
+    
+    fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFuture {
+        match (req.method(), req.uri().path()) {
+            (&Method::GET, "/") => four_oh_four(),
+            (&Method::POST, "/ping") => process_request(req),
+            _ => unimplemented!(),
+            }
+            
     }
+
+    static NOTFOUND: &[u8] = b"Oops! Not Found";
+
+    fn four_oh_four() -> ResponseFuture {
+        let body = Body::from(NOTFOUND);
+        Box::new(future::ok(
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(body)
+                .unwrap(),
+        ))
+    }
+
+    
+    fn process_request(req: Request<Body>) -> ResponseFuture {
+        Box::new(
+            req.into_body()
+                .concat2() // concatenate all the chunks in the body
+                .from_err() // like try! for Result, but for Futures
+                .and_then(|whole_body| {
+                    let str_body = String::from_utf8(whole_body.to_vec()).unwrap();
+                    //let words: Vec<&str> = str_body.split('=').collect();
+                    redirect_home(str_body)
+                }),
+        )
+    }
+
+    fn redirect_home(request: String) -> ResponseFuture {
+        println!("{}", request);
+
+        Box::new(future::ok(
+            Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(serde_json::json!(request).to_string()))
+                .unwrap(),
+        ))
+    }
+
+
+    rt::run(future::lazy(move || {
+        // create a Client for all Services
+        let client = Client::new();
+
+        // define a service containing the router function
+        let new_service = move || {
+            // Move a clone of Client into the service_fn
+            let client = client.clone();
+            service_fn(move |req| router(req, &client))
+        };
+
+        // Define the server - this is what the future_lazy() we're building will resolve to
+        let server = Server::bind(&addr)
+            .serve(new_service)
+            .map_err(|e| eprintln!("Server error: {}", e));
+
+        println!("Listening on http://{}", addr);
+        server
+    }));
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+// fn get_payload_request(){
 
-    println!("{}",String::from_utf8_lossy(&buffer[..]));
-    
-    
-    let get = b"POST /file-check HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "response.html")
-    } else {
+// }
 
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
+// fn verify_webhook_signature() {
 
-    let contents = fs::read_to_string(filename).unwrap();
+// }
 
-    let response = format!("{}{}", status_line, contents);
+// fn authenticate_app() {
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
+// }
 
+// fn authenticate_installation() {
+
+// }
